87313a5927b6aabc0eaefb78f26a432ac84b1897
Vivek Bhatnagar <bhatnagar1990@gmail.com>
2019-11-09 16:04:26 +0100

    Initial set up from spring initialier

---
+++ src/main/java/com/project/springbootappenginestandard/SpringBootAppEngineStandardApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootappenginestandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppEngineStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppEngineStandardApplication.class, args);
+	}
+
+}
